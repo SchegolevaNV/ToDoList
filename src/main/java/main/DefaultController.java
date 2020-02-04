b4b62b9819b20db416d219be2ec743d670bac766
SchegolevaNV <ariwenn15@gmail.com>
2020-02-04 16:41:05 +0300

    change index location

--- src/main/java/main/DefaultController.java
+++ src/main/java/main/DefaultController.java
@@ -3,8 +3,6 @@ package main;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 @RestController
 public class DefaultController
 {
