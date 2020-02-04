8bb14c5ab269aa321d82efed17d4861d41090549
SchegolevaNV <ariwenn15@gmail.com>
2020-02-04 14:53:35 +0300

    add js, css, index.html, classes

--- src/main/java/main/DefaultController.java
+++ src/main/java/main/DefaultController.java
@@ -3,12 +3,14 @@ package main;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class DefaultController
 {
     @RequestMapping("/")
-    public double index()
+    public String index()
     {
-        return Math.random() * 10_000;
+        return (new Date()).toString();
     }
 }
---
+++ src/main/java/response/Book.java
@@ -0,0 +1,32 @@
+package response;
+
+public class Book
+{
+    private int id;
+    private String name;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
