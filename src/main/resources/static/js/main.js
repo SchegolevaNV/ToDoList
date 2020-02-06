765d150b2fc19112b25821868e57938f76a6491d
SchegolevaNV <ariwenn15@gmail.com>
2020-02-06 11:08:28 +0300

    all is working (backend and frontend)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,14 @@
 $(function(){
-    const appendDeal = function(data){
+    const appendDeal = function(data)
+    {
         var dealCode = '<a href="#" class="deal-link" data-id="' +
             data.id + '">' + data.name + '</a>  ';
         var dealRemoveImg =
         '<img src="https://ru.wargaming.net/clans/media/clans/emblems/cl_555/205555/emblem_64x64.png" width="34px" data-id="'
             + data.id + '" class="delete-deal">';
-        var dealChangeImg =
-        '<img src="https://www.riccardofratini.it/wp-content/uploads/2019/02/icons8-edit-64.png" width="34px" data-id="'
-            + data.id + '" class="deal-change">';
         $('#deal-list')
             .append('<div>' + dealCode
             + dealRemoveImg
-            + dealChangeImg
             + '</div><br>');
     };
 
@@ -86,67 +83,61 @@ $(function(){
         return false;
     });
 
-        //Delete one deal
-        $(document).on('click', '.delete-deal', function()
-        {
-              var link = $(this);
-                    var dealId = link.data('id');
-                    $.ajax({
-                        method: "DELETE",
-                        url: '/deals/' + dealId,
-                        success: function(response)
-                        {
-                            alert("Дело № " + dealId +" удалено");
-                            link.parent().remove();
-                        },
-                        error: function(response)
-                        {
-                            if(response.status == 404) {
-                                alert('Такая задача не найдена!');
-                            }
-                        }
-                    });
-                    return false;
+    //Delete one deal
+    $(document).on('click', '.delete-deal', function()
+    {
+        var link = $(this);
+        var dealId = link.data('id');
+        $.ajax({
+            method: "DELETE",
+            url: '/deals/' + dealId,
+            success: function(response)
+            {
+                alert("Дело № " + dealId +" удалено");
+                link.parent().remove();
+            },
+            error: function(response)
+            {
+                if(response.status == 404) {
+                   alert('Такая задача не найдена!');
+                }
+            }
         });
+        return false;
+    });
 
-        //Delete all deals
-        $('#clear-deal-list').click(function()
+    //Delete all deals
+    $('#clear-deal-list').click(function()
+    {
+        var data = $('#deal-list').serialize();
+        $.ajax(
         {
-            var data = $('#deal-list').serialize();
-            $.ajax({
-                    method: "DELETE",
-                    url: '/deals/all',
-                    caches: false,
-                    success: function(response)
-                    {
-                       $('#deal-list').empty();
-                       alert('Все запланированные дела удалены');
-                    },
-                    error: function (response) {
-                        if(response.status !== 200) {
-                           alert('Внутренняя ошибка!');
-                        }
-                    }
-            });
-            return false;
+            method: "DELETE",
+            url: '/deals/all',
+            caches: false,
+            success: function(response)
+            {
+                $('#deal-list').empty();
+                alert('Все запланированные дела удалены');
+            },
+            error: function (response) {
+                if(response.status !== 200) {
+                    alert('Внутренняя ошибка!');
+                }
+            }
         });
+        return false;
+    });
 
-    $('#hide-deal-list').click(function(){
+    //скрыть список дел на странице
+    $('#hide-deal-list').click(function()
+    {
       $('.list').css('display', 'none');
     });
 
-    $('#show-deal-list').click(function(){
+    //показать список дел на странице
+    $('#show-deal-list').click(function()
+    {
       $('.list').css('display', 'block');
     });
-
-    $(document).on('click', '.deal-change', function(){
-      $('#deal-change-form').css('display', 'flex');
-    });
-
-        //Closing adding deal form
-        $(document).on('click', '.deal-change', function(event){
-            if(event.target === this) {
-                $(this).css('display', 'none');
-            }
-        });
 });
